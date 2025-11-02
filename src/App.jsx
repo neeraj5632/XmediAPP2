587f3927f857bb401f9d22d85d2268d406202f74
Neeraj <neerajpande08@gmail.com>
2025-11-03 02:44:39 +0530

    adding some CSS ad style

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Search from "./components/Search";
 import HospitalList from "./components/HospitalList";
 import MyBookings from "./components/MyBookings";
+import "./App.css";
 
 export default function App() {
   return (
